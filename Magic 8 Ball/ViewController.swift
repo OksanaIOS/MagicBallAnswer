12744d45b8db6b1a777f510482d8d1a8697741a2
Oksana ogay <example@email.com>
2024-09-14 14:18:10 +0500

    setup button

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var ballImageView: UIImageView!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
-
-
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+    }
+    @IBAction func buttonAnswer(_ sender: UIButton) {
+        
+        let arrayBallImage = [
+            UIImage(named: "ball1"),
+            UIImage(named: "ball2"),
+            UIImage(named: "ball3"),
+            UIImage(named: "ball4"),
+            UIImage(named: "ball5")
+        ]
+        ballImageView.image = arrayBallImage.randomElement() ?? UIImage()
+    }
+    
+}
+    #Preview {
+        ViewController()
+    
 }
 
